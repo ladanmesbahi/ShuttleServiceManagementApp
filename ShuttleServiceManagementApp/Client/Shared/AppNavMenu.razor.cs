9e2a9b45ba245ebedb2f8c227900797a328835c3
mesbahil <mesbahil@AG1115>
2023-05-28 12:10:25 +0330

    modified styling of nav bar and layout

---
+++ ShuttleServiceManagementApp/Client/Shared/AppNavMenu.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace ShuttleServiceManagementApp.Client.Shared
+{
+    public partial class AppNavMenu
+    {
+        private bool collapseNavMenu = true;
+
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
